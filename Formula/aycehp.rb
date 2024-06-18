141eee759326ba544be66f8075ecabec7f0eda4c
absolutejunkyarddog <173116527+absolutejunkyarddog@users.noreply.github.com>
2024-06-17 18:54:13 -0700

    fix sha256

--- Formula/aycehp.rb
+++ Formula/aycehp.rb
@@ -9,7 +9,7 @@ class Aycehp < Formula
     on_linux do
         if Hardware::CPU.intel?
             url "https://github.com/absolutejunkyarddog/homebrew-private/releases/download/v0.1.0/aycehp-0.1.0-linux-x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-            sha256 "298149d034af3783a08d820e89005fda85bcc43063ad7fe63c11ef5258984fde"
+            sha256 "1ffde98ae6972c009a22e4c2fbfc696042fda0846e2608d794602622408d6d2f"
         elsif Hardware::CPU.arm?
             url "https://github.com/absolutejunkyarddog/homebrew-private/releases/download/v0.1.0/aycehp-0.1.0-linux-arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
             sha256 "0208ed34a343f8d1ed9b6b31140864bee2c5cd12551b4d2e15843786dec94729"
